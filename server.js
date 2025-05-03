9618873aee7b8a77b4cf1ea2c4a52c3543e9c79a
code-star328 <denys.kostiv.dev@gmail.com>
2025-05-03 20:36:07 +0300

    2

--- server.js
+++ server.js
@@ -35,10 +35,7 @@ let browser;
                 '--no-first-run',
                 '--no-zygote',
                 '--single-process'
-            ],
-            executablePath: process.env.NODE_ENV === 'production' 
-                ? '/usr/bin/google-chrome-stable'
-                : undefined
+            ]
         });
         console.log('Browser instance created successfully');
     } catch (error) {
